81baaa2146896f53aedfc01924cd0b25e567be68
S <shreevidya.s-@hpe.com>
2020-03-26 13:31:37 +0530

    get_method_eg.py

---
+++ get_method_eg.py
@@ -0,0 +1,20 @@
+from flask import Flask, redirect, url_for
+app = Flask(__name__)
+
+#statis route
+@app.route("/welcome")
+def hellow():
+    return "Hello world!"
+
+#dynamic route
+@app.route("/<name>")
+def printname(name):
+    return f"hello {name}!"
+
+#to redirect to another page if opened a page that is not for the user(say)
+@app.route("/admin")
+def admin():
+    return redirect(url_for("printname", name="Admin!"))
+
+if(__name__=='__main__'):
+    app.run(debug=True, host="localhost", port= 3000)
